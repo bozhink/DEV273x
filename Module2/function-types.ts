255181435935e8138067735e77c9865f3867ca44
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2017-09-06 19:12:05 +0300

    Update function-types.ts

--- Module2/function-types.ts
+++ Module2/function-types.ts
@@ -1,3 +1,18 @@
 interface SearchFunc {
     (source: string, subString: string): boolean;
 }
+
+let mySearch: SearchFunc = function (source: string, subString: string): boolean {
+    let result: number = source.search(subString);
+    return result > -1;
+}
+
+let mySearch1: SearchFunc = function (src: string, sub: string): boolean {
+    let result: number = src.search(sub);
+    return result > -1;
+}
+
+let mySearch2: SearchFunc = function (src, sub) {
+    let result = src.search(sub);
+    return result > -1;
+}
