8a30bf02b26e9131f5055f2f1e0fa52e5228a65a
Bozhin Karaivanov <bozhink@gmail.com>
2017-09-06 23:09:46 +0300

    Update classes.ts

--- Module2/classes.ts
+++ Module2/classes.ts
@@ -1,10 +1,11 @@
 class Greeter {
+    static standardGreeting: string = "Hello, ";
     greeting: string;
     constructor(message: string) {
         this.greeting = message;
     }
     greet() {
-        return "Hello, " + this.greeting;
+        return Greeter.standardGreeting + this.greeting;
     }
 }
 
@@ -79,3 +80,115 @@ class Octopus1 {
 
 let dad = new Octopus("Man with the 8 strong legs");
 // dad.name = "Man with the 3-piece suit";
+
+class Employee2 {
+    fullName: string;
+}
+
+let employee2 = new Employee2();
+employee2.fullName = "Bob Smith";
+if (employee2.fullName) {
+    console.log(employee2.fullName);
+}
+
+let passcode = "secret passcode";
+
+class Employee3 {
+    private _fullName: string;
+
+    get fullName(): string {
+        return this._fullName;
+    }
+
+    set fullName(newName: string) {
+        if (passcode && passcode === "secret passcode") {
+            this._fullName = newName;
+        } else {
+            console.log("Error: Unauthorized update of employee!");
+        }
+    }
+}
+
+let employee3 = new Employee3();
+employee3.fullName = "Bob Smith";
+if (employee3.fullName) {
+    console.log(employee3.fullName);
+}
+
+
+class Grid {
+    static origin = { x: 0, y: 0 };
+
+    constructor(public scale: number) { }
+
+    calculateDistanceFromOrigin(point: { x: number; y: number; }) {
+        let xDist = point.x - Grid.origin.x;
+        let yDist = point.y - Grid.origin.y;
+        return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
+    }
+}
+
+let grid1 = new Grid(1.0);
+let grid2 = new Grid(5.0);
+
+console.log(grid1.calculateDistanceFromOrigin({ x: 10, y: 10 }));
+console.log(grid2.calculateDistanceFromOrigin({ x: 10, y: 10 }));
+
+
+abstract class Animal2 {
+    abstract makeSound(): void;
+    move(): void {
+        console.log("roaming the earth...");
+    }
+}
+
+abstract class Department {
+    constructor(public name: string) { }
+
+    printName(): void {
+        console.log("Department ma,e: " + this.name);
+    }
+
+    abstract printMeeting(): void;
+}
+
+class AccountingDepartment extends Department {
+    constructor() {
+        super("Accounting and Auditing");
+    }
+
+    printMeeting(): void {
+        console.log("The Accounting Department meets each Monday at 10AM.");
+    }
+
+    generateReports(): void {
+        console.log("Generating accounting reports...");
+    }
+}
+
+let department: Department;
+// department = new Department();
+department = new AccountingDepartment();
+department.printName();
+department.printMeeting();
+// department.generateReports();
+
+
+let greeter1 = new Greeter("1");
+console.log(greeter1.greet());
+
+let greeterMaker: typeof Greeter = Greeter;
+greeterMaker.standardGreeting = "Hey there!";
+let greeter2 = new greeterMaker("2");
+console.log(greeter2.greet());
+
+class Point {
+    x: number;
+    y: number;
+}
+
+interface Point3d extends Point {
+    z: number;
+}
+
+let point3d: Point3d = { x: 1, y: 2, z: 3 };
