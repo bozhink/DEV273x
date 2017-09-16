2b5e3254b18f0a2915ed6803605e4216de7a742f
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2017-09-16 22:53:39 +0300

    Add export-import sample

---
+++ Module3/module1.ts
@@ -0,0 +1,9 @@
+let someString: string = "some string";
+
+export function someMethod() { };
+
+export class someClass { }
+
+export interface someInterface { }
+
+export default someString;
---
+++ Module3/module2.ts
@@ -0,0 +1,4 @@
+import someString1 from './module1';
+import { someMethod, someClass, someInterface } from './module1';
+
+let myString = someString1;
