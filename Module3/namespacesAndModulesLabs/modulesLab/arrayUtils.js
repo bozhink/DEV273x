c0f355be12fa38b08d33a22f24f3e60aa4758e1e
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2017-09-24 18:24:03 +0300

    Module 3: SystemJS + Lodash

--- Module3/namespacesAndModulesLabs/modulesLab/arrayUtils.js
+++ Module3/namespacesAndModulesLabs/modulesLab/arrayUtils.js
@@ -1,20 +1,21 @@
 "use strict";
 exports.__esModule = true;
+var _ = require("lodash");
 var ArrayUtilities = /** @class */ (function () {
     function ArrayUtilities() {
     }
     ArrayUtilities.prototype.reverseArray = function (array) {
-        return array.slice(0).reverse();
+        return _.reverse(array.slice(0));
     };
     ArrayUtilities.prototype.lastItemOfArray = function (array) {
-        return array.slice(0).pop();
+        return _.last(array.slice(0));
     };
     ArrayUtilities.prototype.firstItemOfArray = function (array) {
-        return array.slice(0).shift();
+        return _.head(array.slice(0));
     };
     ArrayUtilities.prototype.concatenateArray = function (array1, array2) {
-        return array1.concat(array2);
+        return _.concat(array1, array2);
     };
     return ArrayUtilities;
 }());
-exports["default"] = ArrayUtilities;
+exports["default"] = new ArrayUtilities;
