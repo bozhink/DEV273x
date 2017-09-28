f4a416e533ac86649a92445f0e166fedb8ca6dfb
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2017-09-28 22:46:40 +0300

    Add adv

---
+++ Module4/advancedTypeScriptLab_async/advancedTypeScriptLab.ts
@@ -0,0 +1,121 @@
+async function getAvatar_Async (elem: HTMLElement) {
+    let uiName = await fetch('https://uinames.com/api/');
+    let avatarName = await uiName.json();
+    alert('Hi! My name is ' + avatarName.name);
+    let avatar = 'https://robohash.org/set_set3/'+ avatarName.name +'?size=60x60' 
+    elem.style.backgroundImage = 'url("' + avatar + '")';
+    document.body.appendChild(elem);
+}
+
+class genericClass<T> {
+    private val: T;
+    public setVal(val: T) {
+        this.val = val;
+    }
+    public getVal(): T {
+        return this.val;
+    }
+}
+
+let element1 = new genericClass<Element>();
+let element2 = new genericClass<HTMLElement>();
+let element3 = new genericClass<Element>();
+
+element1.setVal(document.createElement('div'));
+element2.setVal(document.createElement('div'));
+element3.setVal(document.createElement('div'));
+
+let elementArray = [
+    element1.getVal(),
+    element2.getVal(),
+    element3.getVal()
+];
+
+function isHTMLElement(x: any): x is HTMLElement {
+    return x.style !== undefined;
+}
+
+function convertElement(elem: Element | HTMLElement): HTMLElement {
+    if (!isHTMLElement(elem)) {
+        return elem as HTMLElement;
+    } else {
+        return elem;
+    }
+}
+
+function standardizeElements(elemArray: Array<any>): Array<HTMLElement> {
+    for (let elem of elemArray) {
+        convertElement(elem);
+    }
+
+    return elemArray;
+}
+
+let standardElements = standardizeElements(elementArray);
+
+class Rotater {
+    public rotate(elem: HTMLElement): void {
+        elem.style.transform = "rotate(-315deg)";
+    }
+
+    public rotateBack(elem: HTMLElement): void {
+        elem.style.transform = "";
+    }
+}
+
+class Mover {
+    public move(elem: HTMLElement): void {
+        elem.style.transform = "translateX(50px)";
+    }
+
+    public moveBack(elem: HTMLElement): void {
+        elem.style.transform = "";
+    }
+}
+
+// Mixin
+class movingElement implements Rotater, Mover {
+    rotate: (elem: HTMLElement) => any;
+    move: (elem: HTMLElement) => any;
+    moveBack: (elem: HTMLElement) => any;
+    rotateBack: (elem: HTMLElement) => any;
+    element: HTMLElement;
+    constructor(elem: HTMLElement) {
+        elem.onmousedown = () => {
+            this.move(elem);
+        }
+
+        elem.onmouseup = () => {
+            this.moveBack(elem);
+        }
+
+        elem.onmouseover = () => {
+            this.rotate(elem);
+        }
+
+        elem.onmouseout = () => {
+            this.rotateBack(elem);
+        }
+
+        this.element = elem;
+    }
+}
+
+function applyMixins(derivedClass: any, baseClasses: any[]) {
+    baseClasses.forEach(baseClass => {
+        Object.getOwnPropertyNames(baseClass.prototype).forEach(name => {
+            derivedClass.prototype[name] = baseClass.prototype[name];
+        });
+    });
+}
+
+applyMixins(movingElement, [Mover, Rotater]);
+
+for (let elem of standardElements) {
+    elem.style.width = "60px"
+    elem.style.height = "60px"
+    elem.style.margin = "5px"
+    let elemClass = new movingElement(elem);
+    
+    getAvatar_Async(elemClass.element);
+}
---
+++ Module4/advancedTypeScriptLab_promise/advancedTypeScriptLab.d.ts
@@ -0,0 +1,31 @@
+declare function getAvatar_Promise(elem: HTMLElement): void;
+declare class genericClass<T> {
+    private val;
+    setVal(val: T): void;
+    getVal(): T;
+}
+declare let element1: genericClass<Element>;
+declare let element2: genericClass<HTMLElement>;
+declare let element3: genericClass<Element>;
+declare let elementArray: Element[];
+declare function isHTMLElement(x: any): x is HTMLElement;
+declare function convertElement(elem: Element | HTMLElement): HTMLElement;
+declare function standardizeElements(elemArray: Array<any>): Array<HTMLElement>;
+declare let standardElements: HTMLElement[];
+declare class Rotater {
+    rotate(elem: HTMLElement): void;
+    rotateBack(elem: HTMLElement): void;
+}
+declare class Mover {
+    move(elem: HTMLElement): void;
+    moveBack(elem: HTMLElement): void;
+}
+declare class movingElement implements Rotater, Mover {
+    rotate: (elem: HTMLElement) => any;
+    move: (elem: HTMLElement) => any;
+    moveBack: (elem: HTMLElement) => any;
+    rotateBack: (elem: HTMLElement) => any;
+    element: HTMLElement;
+    constructor(elem: HTMLElement);
+}
+declare function applyMixins(derivedClass: any, baseClasses: any[]): void;
